9a9d98938e6e0b11411df23d73f1362ec7806226
Alyssa <ajmeller@github.com>
2020-12-11 23:28:57 -0500

    works page

--- angular-meller-art-portfolio/src/app/app-routing.module.ts
+++ angular-meller-art-portfolio/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component'
 import { ContactComponent } from './contact/contact.component'
+import { WorksComponent } from './works/works.component'
 
 
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent}
-
+  {path: 'contact', component: ContactComponent},
+  {path: 'works', component: WorksComponent}
 ];
 
 @NgModule({
